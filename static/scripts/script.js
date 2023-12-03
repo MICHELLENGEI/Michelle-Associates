24f28c6d881b984993ac3122df66d3bdb7569d6a
MICHELLLENGEI <michellengei20@gmail.com>
2023-12-03 07:09:33 +0300

    changes

---
+++ static/scripts/script.js
@@ -0,0 +1,10 @@
+function showSection(sectionId) {
+    const sections = document.querySelectorAll('main div');
+    sections.forEach(section => {
+        if (section.id === sectionId) {
+            section.style.display = 'block';
+        } else {
+            section.style.display = 'none';
+        }
+    });
+}
